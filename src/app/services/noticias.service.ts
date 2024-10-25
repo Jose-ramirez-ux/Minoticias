aeff08a85b0c796ac248292827dfb746c82ebdc1
Jose-ramirez-ux <jose_ramirezca@fet.edu.co>
2024-10-25 00:55:21 -0500

    Importacion noticas

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Noticias } from '../interfaces/aritculos';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +9,6 @@ export class NoticiasService {
   constructor(private Http: HttpClient) { }
 
   obtenerNoticias(){
-  return this.Http.get<any>("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f6b4f44bc55a4f52824071837f44352b")  
+  return this.Http.get<Noticias>("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f6b4f44bc55a4f52824071837f44352b")  
   }
 }
